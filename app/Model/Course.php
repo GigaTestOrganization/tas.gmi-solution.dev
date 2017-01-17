47aae8511dfa4ef4017d5180404f49b43d15e903
GigaMareSoftwareDev <arnold.monticalvo@gigamare.com>
2017-01-17 16:12:18 +0800

    Deleted Codes

--- app/Model/Course.php
+++
@@ -1,7 +0,0 @@
-<?php
-	class Course extends AppModel
-	{
-		public $belongsTo = array('CourseCategory', 'User');
-		public $hasMany = array('CourseSpecification', 'CourseLesson', 'Offering');
-		public $virtualFields = array('author' => "(SELECT CONCAT(UserProfile.first_name, ' ', UserProfile.last_name) AS AUTHOR FROM user_profiles AS UserProfile WHERE UserProfile.user_id = Course.user_id LIMIT 1)");
-	}
--- app/Model/Person.php
+++
@@ -1,8 +0,0 @@
-<?php
-	class Person extends AppModel
-	{		
-		public $belongsTo = array("Customer");
-		public $hasMany = array("OfferingParticipant");
-		public $virtualFields = array('encoder_name' => "(SELECT CONCAT(UserProfile.first_name, ' ', UserProfile.last_name) AS AUTHOR FROM user_profiles AS UserProfile WHERE UserProfile.user_id = Person.user_id LIMIT 1)", 'fullname'=>'CONCAT_WS(" ", Person.first_name, Person.last_name)', 'company_name'=>"(SELECT name FROM customers AS Customer WHERE Customer.id = Person.customer_id)");
-	}
-?>
